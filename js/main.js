548d5bd2e99db2fd616d35d7dfff2271ba52769c
pingvin <r-n-b@bk.ru>
2014-03-24 03:16:32 +0400

    page anketa

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 /*http://pingvisha.ru*/
 $(document).ready(function() {
+    $(".radiobtn").on("click", function () {
+        var input_radio = $(this).parent().prev().children("input");
+        input_radio.prop("checked", false);
+        input_radio.eq($(this).index()).prop("checked", true);
+        $(this).parent().children().removeClass("active");
+        $(this).addClass("active");
+    });
 
+    $("input:checked").each(function () {
+       $(this).parent().next().children().eq($(this).index()).addClass("active");
+    });
 });
